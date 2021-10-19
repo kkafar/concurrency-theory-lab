53ee13aaade78d9a66315b0178cb591b86e90f56
Kacper Kafara <kacperkafara@gmail.com>
2021-10-19 09:56:56 +0200

    Test java IO

--- test-module/src/main/Main.java
+++ test-module/src/main/Main.java
@@ -1,16 +1,21 @@
 package main;
 
-import main.custom.ClassWithExpressiveConstructor;
 import main.custom.Monitor;
 
-import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.ThreadPoolExecutor;
+import java.io.*;
+import java.util.Arrays;
 
 public class Main {
   private static final int N_ITER = 10;
   private static final int N_THREADS = 5;
+  private static final int BUFF_SIZE = 5;
+  private static final String DATA_PATH = "test-module/data";
 
-  public static void main(String[] args) throws InterruptedException {
+  public static void main(String[] args) throws InterruptedException, IOException {
+    fileIO(args);
+  }
+
+  public static void simpleMonitor(String[] args) throws InterruptedException {
     Monitor monitorInstance = new Monitor(0);
     Thread[] threadPoolIncrement = new Thread[N_THREADS / 2];
     Thread[] threadPoolDecrement = new Thread[N_THREADS / 2];
@@ -39,5 +44,24 @@ public class Main {
     }
 
     System.out.println("Value: " + monitorInstance.getSharedValue());
+  }
+
+  public static void fileIO(String[] args) throws IOException {
+    File file = new File(DATA_PATH + "/mockData.txt");
+    char[] charBuffer = new char[BUFF_SIZE];
+    Arrays.fill(charBuffer, (char) 0);
+
+    try {
+      FileReader fileReader = new FileReader(file);
+      int charactersRead;
+      while ((charactersRead = fileReader.read(charBuffer, 0, charBuffer.length)) != -1) {
+        String data = String.valueOf(charBuffer);
+        System.out.println(data);
+        Arrays.fill(charBuffer, (char) 0);
+      }
+    } catch (FileNotFoundException fnf) {
+      System.out.print(fnf.getMessage());
+      throw fnf;
+    }
   }
 }
