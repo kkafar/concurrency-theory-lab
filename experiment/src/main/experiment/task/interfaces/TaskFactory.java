a22eed95fbbe3984977f6fcaa218f0c31a6cc45b
Kacper Kafara <kacperkafara@gmail.com>
2021-11-12 17:31:55 +0100

    Add TaskFactory interface

---
+++ experiment/src/main/experiment/task/interfaces/TaskFactory.java
@@ -0,0 +1,15 @@
+package main.experiment.task.interfaces;
+
+import main.actors.interfaces.ConsumerFactory;
+import main.actors.interfaces.ProducerFactory;
+
+public interface TaskFactory {
+  Task create(
+      final String description,
+      final int numberOfProducers,
+      final int numberOfConsumers,
+      final ProducerFactory producerFactory,
+      final ConsumerFactory consumerFactory,
+      final long initialRngSeed
+  );
+}
