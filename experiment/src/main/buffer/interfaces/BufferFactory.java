61a117ac25c577e6b2ff4432ae139481ff35b8d9
Kacper Kafara <kacperkafara@gmail.com>
2021-11-13 14:26:25 +0100

    Add method desciprtion in main.buffer.interfaces package

--- experiment/src/main/buffer/interfaces/BufferFactory.java
+++ experiment/src/main/buffer/interfaces/BufferFactory.java
@@ -1,5 +1,13 @@
 package main.buffer.interfaces;
 
 public interface BufferFactory {
+  /**
+   * Creates instance of buffer
+   *
+   * @param bufferSize maximum buffer capacity
+   * @param actions maximum numbers of put/take operations allowed to execute on a buffer
+   * @param log true if internal information should be logged
+   * @return buffer instance
+   */
   Buffer create(final int bufferSize, final long actions,  boolean log);
 }
