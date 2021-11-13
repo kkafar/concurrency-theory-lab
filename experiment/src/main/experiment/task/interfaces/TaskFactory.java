2dfef87308561044c79d23f3a41d04dae93ca7c2
Kacper Kafara <kacperkafara@gmail.com>
2021-11-13 14:20:14 +0100

    Add method desciprtions in main.task.interfaces package

--- experiment/src/main/experiment/task/interfaces/TaskFactory.java
+++ experiment/src/main/experiment/task/interfaces/TaskFactory.java
@@ -5,6 +5,21 @@ import main.actors.interfaces.ProducerFactory;
 import main.buffer.interfaces.BufferFactory;
 
 public interface TaskFactory {
+  /**
+   * Returns new task instance
+   *
+   * @param description short description of task
+   * @param numberOfProducers number of producer actors that should be started
+   * @param numberOfConsumers number of consumers actors that should be started
+   * @param producerFactory method returning instances of `Producer` abstract class
+   * @param consumerFactory method returning instances of `Consumer` abstract class
+   * @param bufferFactory method returning instances of `Buffer` interface
+   * @param bufferSize maximum size of the buffer shared between actors
+   * @param bufferOperationsBound number of operations that should be executed on buffer
+   * @param startingRngSeed initial seed for random number generator
+   * @param repeats number of times the task should be repeated
+   * @return new, configured task instance
+   */
   Task create(
       final String description,
       final int numberOfProducers,
