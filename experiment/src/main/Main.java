40a04834ed3d0c755004b680d943b0908a67f372
Kacper Kafara <kacperkafara@gmail.com>
2021-11-14 18:08:29 +0100

    Main function update

--- experiment/src/main/Main.java
+++ experiment/src/main/Main.java
@@ -1,37 +1,29 @@
 package main;
 
+import main.buffer.impl.FourCondsBufferProxy;
+import main.experiment.Experiment;
+import main.experiment.task.impl.StandardTask;
 import main.actors.impl.RandomSizePortionConsumer;
 import main.actors.impl.RandomSizePortionProducer;
-import main.buffer.impl.SyncThreeLocksBuffer;
-import main.experiment.task.Task;
-import main.experiment.task.TaskResult;
+import main.buffer.impl.ThreeLocksBufferProxy;
+import main.experiment.task.impl.StandardTaskResult;
 
 public class Main {
   private static final int N_PRODUCERS = 3;
   private static final int N_CONSUMERS = 3;
-  private static final int N_REPEATS = 10;
   private static final int BUFFER_SIZE = 10;
-  private static final int N_ITERATIONS = 20;
-  private static final int N_ACTIONS = 100000;
+  private static final int BUFFER_OPS = 10;
+  private static final int N_REPEATS = 1;
   private static final int RNG_SEED = 10;
 
   public static void main(String[] args) throws InterruptedException {
-    Task taskThreeLocks = new Task(
-        "Solution with three locks",
-        RandomSizePortionProducer::new,
-        RandomSizePortionConsumer::new,
-        SyncThreeLocksBuffer::new,
-        RNG_SEED
-    ).configure(
-        N_PRODUCERS,
-        N_CONSUMERS,
-        N_REPEATS,
-        BUFFER_SIZE
-    ).setBufferLog(
-        true
+    Experiment experiment = new Experiment(
+      ThreeLocksBufferProxy::new,
+      RandomSizePortionProducer::new,
+      RandomSizePortionConsumer::new,
+      StandardTask::new,
+      RNG_SEED,
+      N_REPEATS
     );
-    taskThreeLocks.conduct();
-
-    TaskResult experimentThreeLocksResult = taskThreeLocks.getTaskResult();
   }
 }
