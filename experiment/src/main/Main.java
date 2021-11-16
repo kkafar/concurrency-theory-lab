559fd792dfa473f57f1f16e6036625516c1adaec
Kacper Kafara <kacperkafara@gmail.com>
2021-11-16 16:59:59 +0100

    Prepare Main for launch

--- experiment/src/main/Main.java
+++ experiment/src/main/Main.java
@@ -3,11 +3,10 @@ package main;
 import main.buffer.impl.FourCondsBufferProxy;
 import main.experiment.Experiment;
 import main.experiment.task.impl.StandardTask;
-import main.actors.impl.RandomSizePortionConsumer;
-import main.actors.impl.RandomSizePortionProducer;
+import main.actors.impl.RandomPortionConsumer;
+import main.actors.impl.RandomPortionProducer;
 import main.buffer.impl.ThreeLocksBufferProxy;
 import main.experiment.task.impl.StandardTaskConfiguration;
-import main.experiment.task.impl.StandardTaskResult;
 
 public class Main {
   private static final int N_PRODUCERS = 3;
@@ -16,6 +15,22 @@ public class Main {
   private static final int BUFFER_OPS = 10;
   private static final int N_REPEATS = 2;
   private static final int RNG_SEED = 10;
+
+  private static final int[] N_PRODUCERS_ARR = {
+      1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+  };
+
+  private static final int[] N_CONSUMERS_ARR = {
+      1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+  };
+
+  private static final int[] BUFFER_SIZE_ARR = {
+      5, 10, 15, 20, 25, 30, 35, 40, 45, 50
+  };
+
+  private static final int[] BUFFER_OPS_ARR = {
+      1000, 5000, 10000, 15000, 20000, 25000
+  };
 
   private static final boolean log = true;
 
@@ -28,8 +43,8 @@ public class Main {
   private static void conductThreeLocksExperiment() {
     Experiment experiment = new Experiment(
         ThreeLocksBufferProxy::new,
-        RandomSizePortionProducer::new,
-        RandomSizePortionConsumer::new,
+        RandomPortionProducer::new,
+        RandomPortionConsumer::new,
         StandardTask::new,
         RNG_SEED,
         N_REPEATS
@@ -50,8 +65,8 @@ public class Main {
   private static void conductFourCondsExperiment() {
     Experiment experiment = new Experiment(
         FourCondsBufferProxy::new,
-        RandomSizePortionProducer::new,
-        RandomSizePortionConsumer::new,
+        RandomPortionProducer::new,
+        RandomPortionConsumer::new,
         StandardTask::new,
         RNG_SEED,
         N_REPEATS
