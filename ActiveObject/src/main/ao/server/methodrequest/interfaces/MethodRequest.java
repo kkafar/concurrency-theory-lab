42a8c321d82bfeaa62ae8e102894b781de2eda1a
Kacper Kafara <kacperkafara@gmail.com>
2021-11-19 06:19:29 +0100

    Make MethodRequest implement Comparable

--- ActiveObject/src/main/ao/server/methodrequest/interfaces/MethodRequest.java
+++ ActiveObject/src/main/ao/server/methodrequest/interfaces/MethodRequest.java
@@ -2,11 +2,14 @@ package main.ao.server.methodrequest.interfaces;
 
 import main.ao.struct.impls.Promise;
 
-abstract public class MethodRequest<T> {
+abstract public class MethodRequest<T> implements Comparable<MethodRequest<T>> {
   protected String methodName;
   protected Promise<T> promise;
 
+  protected int priority;
+
   public MethodRequest(Promise<T> promise) {
+    this.priority = 1; // TODO
     this.promise = promise;
   }
 
@@ -16,5 +19,10 @@ abstract public class MethodRequest<T> {
 
   public Promise<T> getPromise() {
     return promise;
+  }
+
+  @Override
+  public int compareTo(MethodRequest<T> methodRequest) {
+    return this.priority - methodRequest.priority;
   }
 }
