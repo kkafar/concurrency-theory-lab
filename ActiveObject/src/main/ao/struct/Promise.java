e42aaab81f2b1fb537c1a702f8729c40d0579ff7
Kacper Kafara <kacperkafara@gmail.com>
2021-11-19 03:46:41 +0100

    Add Promise class

---
+++ ActiveObject/src/main/ao/struct/Promise.java
@@ -0,0 +1,42 @@
+package main.ao.struct;
+
+public class Promise<T> {
+  T value;
+  private boolean isResolved;
+  private boolean isRejected;
+
+  public Promise() {
+    this.value = null;
+    this.isResolved = false;
+    this.isRejected = false;
+  }
+  
+  public void resolve(T value) {
+    if (isNotConsumed()) {
+      this.value = value;
+      isResolved = true;
+    }
+  }
+
+  public void reject() {
+    if (isNotConsumed()) {
+      isRejected = true;
+    }
+  }
+
+  public boolean isNotConsumed() {
+    return !isResolved && !isRejected;
+  }
+
+  public boolean isConsumed() {
+    return isRejected || isResolved;
+  }
+
+  public boolean isRejected() {
+    return isRejected;
+  }
+
+  public boolean isResolved() {
+    return isResolved;
+  }
+}
