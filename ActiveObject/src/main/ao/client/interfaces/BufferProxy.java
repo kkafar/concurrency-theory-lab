478bc74d1daafe935d56f1f5e1b2cca1dabd462d
Kacper Kafara <kacperkafara@gmail.com>
2021-11-19 03:59:19 +0100

    Add BufferProxy interface

--- ActiveObject/src/main/ao/client/interfaces/BufferProxy.java
+++ ActiveObject/src/main/ao/client/interfaces/BufferProxy.java
@@ -1,4 +1,8 @@
 package main.ao.client.interfaces;
 
+import main.ao.struct.Promise;
+
 public interface BufferProxy {
+  Promise<Boolean> put(final int portionSize);
+  Promise<Object[]> take(final int portionSize);
 }
