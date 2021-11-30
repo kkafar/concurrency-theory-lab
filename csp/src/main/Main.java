d41a92ae9fb41362b1e6e53e4ff25d4b3e4977d8
Kacper Kafara <kacperkafara@gmail.com>
2021-11-30 15:29:40 +0100

    Implement invalid (for now) Main class (:D)

--- csp/src/main/Main.java
+++ csp/src/main/Main.java
@@ -1,9 +1,26 @@
 package main;
 
+import main.actors.Consumer;
+import main.actors.Producer;
+import org.jcsp.lang.CSProcess;
 import org.jcsp.lang.Channel;
+import org.jcsp.lang.One2OneChannelInt; // TO JEST INTERFEJS A NIE IMPLEMENTACJA
+import org.jcsp.lang.Parallel;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world");
+    new Main();
+  }
+
+  public Main() {
+    final One2OneChannelInt communicationChannel = new One2OneChannelInt();
+
+    CSProcess[] procList = {
+        new Producer(communicationChannel),
+        new Consumer(communicationChannel)
+    };
+
+    Parallel parallel = new Parallel(procList);
+     
   }
 }
