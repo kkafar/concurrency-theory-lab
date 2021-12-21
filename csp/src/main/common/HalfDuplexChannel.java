543d45c4b65b021c51a1b892fc7930412a3ca90c
Kacper Kafara <kacperkafara@gmail.com>
2021-12-21 17:50:04 +0100

    Implement HalfDuplexChannel

--- csp/src/main/common/HalfDuplexChannel.java
+++ csp/src/main/common/HalfDuplexChannel.java
@@ -1,14 +1,47 @@
 package main.common;
 
 import org.jcsp.lang.AltingChannelInput;
-import org.jcsp.lang.AltingChannelOutput;
+import org.jcsp.lang.Channel;
+import org.jcsp.lang.ChannelOutput;
+import org.jcsp.lang.One2OneChannel;
 
-public class HalfDuplexPipe {
-//  private final AltingChannelInput mInput;
-//  private final AltingChannelOutput mOutput;
+import java.util.HashMap;
+import java.util.Map;
 
-  public HalfDuplexPipe() {
-//    mInput = new AltingChannelInput();
-//    mOutput = new AltingChannelOutput();
+public class HalfDuplexChannel {
+  private final One2OneChannel mMasterToSlave;
+  private final One2OneChannel mSlaveToMaster;
+
+  private final Object mMaster, mSlave;
+
+//  private final Map<Object, One2OneChannel> map;
+
+  public HalfDuplexChannel(Object master, Object slave) {
+    assert master != null && slave != null;
+    mMaster = master;
+    mSlave = slave;
+    mMasterToSlave = Channel.one2one();
+    mSlaveToMaster = Channel.one2one();
+//    map = Map.of(master, mMasterToSlave, slave, mSlaveToMaster);
+  }
+
+  public ChannelOutput writeEndpointFor(Object who) {
+    if (who.equals(mMaster)) {
+      return mMasterToSlave.out();
+    } else if (who.equals(mSlave)) {
+      return mSlaveToMaster.out();
+    } else {
+      throw new IllegalArgumentException("Passed object is neither master or slave!");
+    }
+  }
+
+  public AltingChannelInput readEndpointFor(Object who) {
+    if (who.equals(mMaster)) {
+      return mSlaveToMaster.in();
+    } else if (who.equals(mSlave)) {
+      return mMasterToSlave.in();
+    } else {
+      throw new IllegalArgumentException("Passed object is neither master or slave!");
+    }
   }
 }
