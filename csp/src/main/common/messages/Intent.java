2d4a3c85951bf905c18c0b63591a8660edd1bf4f
Kacper Kafara <kacperkafara@gmail.com>
2021-12-21 17:33:32 +0100

    Add Intent class

---
+++ csp/src/main/common/messages/Intent.java
@@ -0,0 +1,33 @@
+package main.common.messages;
+
+import main.actors.Actor;
+
+public class Intent {
+  private final RequestType mRequestType;
+  private final int mResources;
+  private final Actor mActor;
+
+
+  public Intent(
+      RequestType requestType,
+      int resources,
+      Actor actor
+  ) {
+    mResources = resources;
+    mRequestType = requestType;
+    mActor = actor;
+  }
+
+
+  public RequestType getRequestType() {
+    return mRequestType;
+  }
+
+  public int getResources() {
+    return mResources;
+  }
+
+  public Actor getActor() {
+    return mActor;
+  }
+}
