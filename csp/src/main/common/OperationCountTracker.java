082f995cef460d09df735b2d3ffd4734440eb48d
Kacper Kafara <kacperkafara@gmail.com>
2021-12-22 14:25:36 +0100

    Add OperationCountTracker class

--- csp/src/main/common/OperationCountTracker.java
+++ csp/src/main/common/OperationCountTracker.java
@@ -1,5 +1,37 @@
 package main.common;
 
-public interface CompletedOperationCountTracker {
-  int getCompletedOperations();
+public class OperationCountTracker {
+  protected int mCompletedOperations;
+  protected int mRejectedOperations;
+  protected int mFailedOperations;
+
+  public OperationCountTracker() {
+    mCompletedOperations = 0;
+    mRejectedOperations = 0;
+    mFailedOperations = 0;
+  }
+
+  public int getNumberOfCompletedOperations() {
+    return mCompletedOperations;
+  }
+
+  public int getNumberOfRejectedOperations() {
+    return mRejectedOperations;
+  }
+
+  public int getNumberOfFailedOperations() {
+    return mRejectedOperations;
+  }
+
+  public void reportCompletedOperation() {
+    ++mCompletedOperations;
+  }
+
+  public void reportRejectedOperation() {
+    ++mRejectedOperations;
+  }
+
+  public void reportFailedOperation() {
+    ++mFailedOperations;
+  }
 }
